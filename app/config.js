d726fa7e814c58a3dfc5e25631fd768b1df55a2c
Daniele Del Giudice <dunox1@gmail.com>
2017-02-26 11:02:15 +0000

    Changed mongolab uri

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   app_url: process.env.APP_URL || 'https://freecodecamp-dunox1.c9users.io/',
-  mongo_url: process.env.MONGOLAB_URI || 'mongodb://localhost/shurli'
+  mongo_url: process.env.MONGODB_URI || 'mongodb://localhost/shurli'
 };
